e22d8f25622c76c7d8969a7e7cec61bbf8fd21c6
Felipe Johnny <felipijohnny@outlook.com>
2024-06-09 14:38:34 -0300

    aplicando https na api

--- server.js
+++ server.js
@@ -4,9 +4,18 @@ const path =require('path')
 
 const PORT = 3333;
 
+const https = require('https');
+const fs = require('fs');
+
+ const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/felipejohnny.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/felipejohnny.com/fullchain.pem')
+}; 
 //necessario para que as imagens sejam acessiveis via caminho ex: 'http://localhost:3000/public/uploads/images/1712918282155_img.png'
 app.use('/public/uploads/images',express.static(path.join('public/uploads/images')))
 
-app.listen(PORT,()=>{
+const server = https.createServer(httpsOptions,app);
+
+server.listen(PORT,()=>{
     console.log("Servidor ligado na porta: " + PORT );
 })
